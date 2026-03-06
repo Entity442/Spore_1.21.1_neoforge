c4f72b51091d702598981cb6ba3b81464a89a1de
mihai <mihailungoci87@gmail.com>
2026-03-06 00:35:28 -0800

    stuff

--- src/main/java/com/Harbinger/Spore/Client/Layers/GrakenShipLayer.java
+++ src/main/java/com/Harbinger/Spore/Client/Layers/GrakenShipLayer.java
@@ -8,9 +8,9 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 
 public class GrakenShipLayer<T extends Grakensenker> extends RenderLayer<T, GrakensenkerModel<T>> {
@@ -29,9 +29,9 @@ public class GrakenShipLayer<T extends Grakensenker> extends RenderLayer<T, Grak
         }
         this.getParentModel().copyPropertiesTo(model);
         model.prepareMobModel(t, v, v1, v5);
-        model.setupAnim(t, v, v1, v2, v3, v4);
+        model.setupAnim(t, v, v1, v2+t.tickCount, v3, v4);
         renderColoredCutoutModel(model, SHIP, poseStack, multiBufferSource, i, t, -1);
         VertexConsumer vertexconsumer = multiBufferSource.getBuffer(RenderType.eyes(SHIP_EYE));
-        model.renderToBuffer(poseStack, vertexconsumer, i, LivingEntityRenderer.getOverlayCoords(t, 0.0F), 1);
+        model.renderToBuffer(poseStack, vertexconsumer, 15728640, OverlayTexture.NO_OVERLAY, -1);
     }
 }
