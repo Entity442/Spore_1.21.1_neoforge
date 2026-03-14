a98abf8da49d13f7927fc6486a9dc26e6fc01d21
mihai <mihailungoci87@gmail.com>
2026-03-14 08:35:13 -0700

    lighting muhahaa

--- src/main/java/com/Harbinger/Spore/Sentities/Utility/Illusion.java
+++ src/main/java/com/Harbinger/Spore/Sentities/Utility/Illusion.java
@@ -22,6 +22,7 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.FloatGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Enemy;
 import net.minecraft.world.level.Level;
@@ -89,6 +90,7 @@ public class Illusion extends UtilityEntity implements ArmorPersentageBypass, En
         this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>
                 (this, LivingEntity.class,  true,livingEntity -> {
                     return !Illusion.this.getSeeAble() && livingEntity.getId() == getTargetId();}));
+        this.goalSelector.addGoal(1, new FloatGoal(this));
         this.goalSelector.addGoal(3,new CustomMeleeAttackGoal(this,1.3,true));
     }
 
