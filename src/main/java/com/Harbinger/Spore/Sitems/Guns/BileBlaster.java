7b2bc06aef3d7923dd43d9ab9f3e5244b69bb039
mihai <mihailungoci87@gmail.com>
2026-03-21 08:30:21 -0700

    guns and troubles

--- src/main/java/com/Harbinger/Spore/Sitems/Guns/BileBlaster.java
+++ src/main/java/com/Harbinger/Spore/Sitems/Guns/BileBlaster.java
@@ -50,7 +50,7 @@ public class BileBlaster extends AbstractSporeGun implements CustomModelArmorDat
 
     @Override
     public Item getAmmoItem() {
-        return Sitems.BUCKET_OF_BILE.asItem();
+        return Sitems.CRUSTED_BILE.asItem();
     }
 
     @Override
