17395c9db54cbb94d737665b62d5be4e501705e0
mihai <mihailungoci87@gmail.com>
2026-03-22 12:35:11 -0700

    guns and troubles

--- src/main/java/com/Harbinger/Spore/core/Sitems.java
+++ src/main/java/com/Harbinger/Spore/core/Sitems.java
@@ -434,10 +434,6 @@ public class Sitems {
             AcidicAssasin::new);
     public  static final DeferredItem<Item> VIGIL_EYE = ITEMS.register("vigil_eye",
             VigilEye::new);
-    public  static final DeferredItem<Item> BILE_VIAL = ITEMS.register("bile_vial",
-            () -> new BaseItem(new Item.Properties()));
-    public  static final DeferredItem<Item> ACID_VIAL = ITEMS.register("acid_vial",
-            () -> new BaseItem(new Item.Properties()));
     public static final DeferredItem<Item> SYMBIOTIC_REAGENT = ITEMS.register("symbiotic_reagent",
             () -> new BiologicalReagent(BiologicalReagent.AcceptedTypes.ALL_TYPES,Senchantments.SYMBIOTIC_RECONSTITUTION));
     public  static final DeferredItem<Item> CRYOGENIC_REAGENT = ITEMS.register("cryogenic_reagent",
@@ -523,6 +519,10 @@ public class Sitems {
             SharpeningAgent::new);
     public  static final DeferredItem<Item> INTEGRATING_AGENT = ITEMS.register("integrating_agent",
             ConnectingAgent::new);
+    public  static final DeferredItem<Item> BILE_VIAL = ITEMS.register("bile_vial",
+            () -> new BaseItem2(new Item.Properties()));
+    public  static final DeferredItem<Item> ACID_VIAL = ITEMS.register("acid_vial",
+            () -> new BaseItem2(new Item.Properties()));
     public  static final DeferredItem<Item> MUTATION_SYRINGE = ITEMS.register("mutation_syringe",
             MutationSyringe::new);
     public  static final DeferredItem<Item> SYRINGE = ITEMS.register("syringe",
