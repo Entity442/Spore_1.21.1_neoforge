935fdfcbb998ca7a433333d2a45010707cc06514
mihai <mihailungoci87@gmail.com>
2026-03-26 03:07:49 -0700

    guns and troubles

--- src/main/java/com/Harbinger/Spore/Sentities/EvolvedInfected/Gargoyl.java
+++ src/main/java/com/Harbinger/Spore/Sentities/EvolvedInfected/Gargoyl.java
@@ -185,7 +185,7 @@ public class Gargoyl extends EvolvedInfected implements FlyingInfected, ArmedInf
         @Override
         public boolean canUse() {
             target = gargoyle.getTarget();
-            return target != null && target.isAlive() && gargoyle.distanceTo(target) < 32;
+            return target != null && target.isAlive() && gargoyle.distanceTo(target) < 32 && !target.isInWater();
         }
 
         @Override
@@ -245,5 +245,13 @@ public class Gargoyl extends EvolvedInfected implements FlyingInfected, ArmedInf
             state = 0;
             target = null;
         }
+    }
+
+    @Override
+    public boolean hasLineOfSight(Entity entity) {
+        if (entity.isInWater()){
+            return false;
+        }
+        return super.hasLineOfSight(entity);
     }
 }
