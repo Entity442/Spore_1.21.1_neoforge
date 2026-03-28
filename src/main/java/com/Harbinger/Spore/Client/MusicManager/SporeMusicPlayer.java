f4ebaecc499a88a3f393176fbdc31e6acbd6bec9
mihai <mihailungoci87@gmail.com>
2026-03-28 05:31:45 -0700

    tweaks

--- src/main/java/com/Harbinger/Spore/Client/MusicManager/SporeMusicPlayer.java
+++ src/main/java/com/Harbinger/Spore/Client/MusicManager/SporeMusicPlayer.java
@@ -132,7 +132,9 @@ public class SporeMusicPlayer {
 
     public static void handlePacket(boolean pro, int id, boolean inCombat) {
         if (pro && id == 3){
-            currentMusic = null;
+            if (currentMusic != null){
+                currentMusic.fadeOut();
+            }
             playMusic(Ssounds.SOMETHING_ONCE_GREAT.value());
             return;
         }
