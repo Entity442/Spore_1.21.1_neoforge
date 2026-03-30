28a9e1ddbcb83ef4a3e96638869da6dce7dea987
mihai <mihailungoci87@gmail.com>
2026-03-30 00:01:32 -0700

    tweaks

--- src/main/java/com/Harbinger/Spore/Sentities/EvolvedInfected/Gargoyl.java
+++ src/main/java/com/Harbinger/Spore/Sentities/EvolvedInfected/Gargoyl.java
@@ -237,7 +237,7 @@ public class Gargoyl extends EvolvedInfected implements FlyingInfected, ArmedInf
 
         @Override
         public boolean canContinueToUse(){
-            return state != 2;
+            return state != 2 && gargoyle.isAlive();
         }
 
         @Override
