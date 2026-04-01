0cfcff064191123547a3097ee4fa2efc14ee795a
mihai <mihailungoci87@gmail.com>
2026-04-01 02:27:20 -0700

    tweaks

--- src/main/java/com/Harbinger/Spore/Sitems/Agents/ConnectingAgent.java
+++ src/main/java/com/Harbinger/Spore/Sitems/Agents/ConnectingAgent.java
@@ -14,11 +14,15 @@ public class ConnectingAgent extends MutationAgents{
     public void mutateWeapon(ItemStack stack) {
         if (stack.getItem() instanceof SporeWeaponData item){
             int i =1+ SConfig.SERVER.agent_enchantability.get();
-            item.setLuck(this.source.nextInt(1,i),stack);
+            if (i > 2){
+                item.setLuck(this.source.nextInt(1,i),stack);
+            }
         }
         if (stack.getItem() instanceof SporeArmorData item){
             int i =1+ SConfig.SERVER.agent_enchantability.get();
-            item.setLuck(this.source.nextInt(1,i),stack);
+            if (i > 2){
+                item.setLuck(this.source.nextInt(1,i),stack);
+            }
         }
     }
 }
