0112756efedf26d2be471a340e269a471e641970
mihai <mihailungoci87@gmail.com>
2026-04-02 08:27:27 -0700

    tweaks

--- src/main/java/com/Harbinger/Spore/Sentities/Calamities/Hohlfresser.java
+++ src/main/java/com/Harbinger/Spore/Sentities/Calamities/Hohlfresser.java
@@ -328,6 +328,7 @@ public class Hohlfresser extends Calamity implements TrueCalamity, RangedAttackM
                     if (instance != null && hostInstance != null && instance.getValue() != hostInstance.getValue()){
                         instance.setBaseValue(hostInstance.getBaseValue());
                     }
+                    hohlMultipart.setHealth(this.getHealth());
                 }
             }
 
