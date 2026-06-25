4357bd646d216266d4fedc14dceb804ea7ff34bc
mihai <mihailungoci87@gmail.com>
2026-06-25 14:12:28 -0700

    terroriser work

--- src/main/java/com/Harbinger/Spore/Sentities/Projectile/GunProjectiles/ToxinBullet.java
+++ src/main/java/com/Harbinger/Spore/Sentities/Projectile/GunProjectiles/ToxinBullet.java
@@ -51,7 +51,7 @@ public class ToxinBullet extends AbstractGunProjectile {
         SporeToolsMutations mutations = this.getMutationVariant();
         for (Entity entity : entityList){
             if (entity instanceof LivingEntity livingEntity){
-                livingEntity.addEffect(mutations == SporeToolsMutations.ROTTEN ? new MobEffectInstance(MobEffects.POISON,200,3) : new MobEffectInstance(MobEffects.POISON,100,1));
+                livingEntity.addEffect(mutations == SporeToolsMutations.ROTTEN ? new MobEffectInstance(MobEffects.WITHER,200,3) : new MobEffectInstance(MobEffects.WITHER,100,1));
                 livingEntity.addEffect(new MobEffectInstance(MobEffects.WEAKNESS,100,1));
                 if (mutations == SporeToolsMutations.ROTTEN){
                     livingEntity.addEffect(new MobEffectInstance(MobEffects.POISON,100,1));
